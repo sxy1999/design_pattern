c79fe8ed52f4d050a56ea83665a741ded77a040e
Tom Sun <tsun@donbest.com>
2016-06-15 16:13:16 -0700

    Interpreter pattern added.

---
+++ src/com/donbest/behavioral/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package com.donbest.behavioral.interpreter;
+
+public interface Expression {
+	public boolean interpret(String context);
+}
---
+++ src/com/donbest/behavioral/interpreter/InterpreterDemo.java
@@ -0,0 +1,31 @@
+package com.donbest.behavioral.interpreter;
+
+public class InterpreterDemo {
+
+	static Expression buildInterpreter() {
+		Expression terminal1 = new TerminalExpression("Lions");
+		Expression terminal2 = new TerminalExpression("Tigers");
+		Expression terminal3 = new TerminalExpression("Bears");
+
+		// Tigers and bears
+		Expression alternation1 = new AndExpression(terminal2, terminal3);
+
+		// Lions or (Tigers and bears)
+		Expression alternation2 = new OrExpression(terminal1, alternation1);
+
+		return new AndExpression(terminal3, alternation2);
+	}
+
+	public static void main(String[] args) {
+		// String context = "Lions";
+		// String context = "Tigers";
+		// String context = "Bears";
+		// String context = "Lions Tigers";
+//		 String context = "Lions Bears";
+		 String context = "Tigers Bears";
+
+		Expression define = buildInterpreter();
+		System.out.println(context + " is " + define.interpret(context));
+	}
+
+}

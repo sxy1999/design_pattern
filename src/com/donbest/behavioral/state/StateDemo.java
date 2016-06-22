77c2da67a44605262a4daf382b4631526257fc10
Tom Sun <tsun@donbest.com>
2016-06-22 15:29:57 -0700

    added state pattern.

---
+++ src/com/donbest/behavioral/state/StateDemo.java
@@ -0,0 +1,23 @@
+package com.donbest.behavioral.state;
+
+public class StateDemo {
+
+	public static void main(String[] args) {
+		Fan fan = new Fan();
+
+		System.out.println(fan);
+
+		fan.pullChain();
+		System.out.println(fan);
+
+		fan.pullChain();
+		System.out.println(fan);
+		
+		fan.pullChain();
+		System.out.println(fan);
+		
+		fan.pullChain();
+		System.out.println(fan);
+	}
+
+}
